6e015c563d5d5eff5c06391b0495d4bc0dee2dc2
Vladislav Lozhkin <vladislavlozhkin@gmail.com>
2016-10-13 01:02:33 +0500

    Рефакторинг, формирование полного класса модификатора вынесено в метод

--- src/main.js
+++ src/main.js
@@ -5,42 +5,130 @@ class Block {
         this.modSeparator = "_";
     }
 
+    /**
+     * Добавить к ноде дополнительные свойтва и методы
+     * @element {DOM node}
+     */
+    addElementMethodsAndProperties(element) {
+        element.name = name;
+        element.addMod = this.addMod;
+        element.removeMod = this.removeMod;
+        element.toggleMod = this.toggleMod;
+        element.hasMod = this.hasMod;
+        element.blockCtx = this;
+
+        return element;
+    }
+
+    /**
+     * Получить получить полный класс модификатора (вместе с именем блока или элемента)
+     *
+     * @name    {string} Имя блока или элемента
+     * @modName {string} Имя модификатора
+     * @value   {string} значение модификатора
+     * @return  {string}
+     *
+     * TODO реализовать добавление значения модификатора к modFullName
+     */
+    getFullModClassName(name, modName, value = null) {
+        let modFullClassName = this.blockName +
+                               this.elementSeparator +
+                               name +
+                               this.modSeparator +
+                               modName;
+
+        return modFullClassName;
+    }
+
+    /**
+     * Получить элемент по имени
+     * @name {string} имя блока
+     */
     element(name) {
         let selector = '.' +  this.blockName + this.elementSeparator + name;
         let node = document.querySelector(selector);
+        let element = this.addElementMethodsAndProperties(node);
 
-        node.name = name;
-        node.addMod = this.addMod;
-        node.removeMod = this.removeMod;
-        node.toggleMod = this.toggleMod;
-        node.isMod = this.isMod;
-        node.blockCtx = this;
-
-        return node;
+        return element;
     }
 
+    /**
+     * Получить элементы блока по имени
+     * @name {string}
+     */
     elements(name) {
         console.log(name);
     }
 
-    addMod(name, value = null) {
-        let modFullName = this.blockCtx.blockName + this.blockCtx.elementSeparator + this.name + this.blockCtx.modSeparator + name;
+    /**
+     * Добавить к модификатор к блоку/элементу
+     * @name  {string} имя модификатора
+     * @value {string} значение модификатора
+     */
+    addMod(name, value) {
+        let modFullName = this.blockCtx.getFullModClassName(this.name, name, value);
         this.classList.add(modFullName);
     }
 
-    removeMod(name, value = null) {
-        let modFullName = this.blockCtx.blockName + this.blockCtx.elementSeparator + this.name + this.blockCtx.modSeparator + name;
+    /**
+     * Удалить модификатор у блока/элемента
+     * @name  {string} имя модификатора
+     * @value {string} значение модификатора
+     */
+    removeMod(name, value) {
+        let modFullName = this.blockCtx.getFullModClassName(this.name, name, value);
         this.classList.remove(modFullName);
     }
 
-    toggleMod(name, value = null) {
-        let modFullName = this.blockCtx.blockName + this.blockCtx.elementSeparator + this.name + this.blockCtx.modSeparator + name;
+    /**
+     * Переключатель модификатора блока.
+     * Добавит мод если его нет и удалит в противном случае
+     * @name  {string} имя модификатора
+     * @value {string} значение модификатора
+     */
+    toggleMod(name, value) {
+        let modFullName = this.blockCtx.getFullModClassName(this.name, name, value);
         this.classList.toggle(modFullName);
     }
 
-    isMod(name, value = null) {
-        let modFullName = this.blockCtx.blockName + this.blockCtx.elementSeparator + this.name + this.blockCtx.modSeparator + name;
-        console.log(modFullName);
+    /**
+     * Проверка - есть ли у блока модификатор
+     * @name   {string}  имя модификатора
+     * @value  {string}  значение модификатора
+     */
+    toggleMod(name, value) {
+        let modFullName = this.blockCtx.getFullModClassName(this.name, name, value);
+        this.classList.toggle(modFullName);
+    }
+
+    /**
+     * Проверка - есть ли у блока модификатор
+     * @name   {string}  имя модификатора
+     * @value  {string}  значение модификатора
+     */
+    toggleMod(name, value) {
+        let modFullName = this.blockCtx.getFullModClassName(this.name, name, value);
+        this.classList.toggle(modFullName);
+    }
+
+    /**
+     * Проверка - есть ли у блока модификатор
+     * @name   {string}  имя модификатора
+     * @value  {string}  значение модификатора
+     */
+    toggleMod(name, value = null) {
+        let modFullName = this.blockCtx.getFullModClassName(this.name, name, value);
+        this.classList.toggle(modFullName);
+    }
+
+    /**
+     * Проверка - есть ли у блока модификатор
+     * @name   {string}  имя модификатора
+     * @value  {string}  значение модификатора
+     * @return {boolean}
+     */
+    hasMod(name, value = null) {
+        let modFullName = this.blockCtx.getFullModClassName(this.name, name, value);
         return this.classList.contains(modFullName);
     }
 }
@@ -55,9 +143,9 @@ class MyBlock extends Block {
         input.addMod('name');
         input.addMod('name_2');
         input.removeMod('name_2');
-        console.log(input.isMod('name_2'));
+        console.log(input.hasMod('name_2'));
         input.toggleMod('name_2');
-        console.log(input.isMod('name_2'));
+        console.log(input.hasMod('name_2'));
     }
 }
 
